d95ebeb9dcb6f2ba4198d70613b48878e16a596e
Brian Chei <157544112+brianchei@users.noreply.github.com>
2025-07-03 14:31:19 +0900

    feat: add operation functionality

--- main.js
+++ main.js
@@ -2,6 +2,11 @@
 Pseudocode
 */
 
+// universal variables
+let operand1;
+let operator;
+let operand2
+
 // arithmetic methods
 const add = function(addend, addend2) {
 	let sum = addend + addend2;
@@ -23,8 +28,35 @@ const divide = function(dividend, divisor) {
     return quotient;
 };
 
+// operation methods
+const operate = function(operand1, operator, operand2) {
+    switch (operator) {
+        case '+':
+            return add(operand1, operand2);
+            break;
+        case '-':
+            return subtract(operand1, operand2);
+            break;
+        case '*':
+            return multiply(operand1, operand2);
+            break;
+        case '/':
+            return divide(operand1, operand2);
+            break;
+        default:
+            throw new Error('Unrecognized operator, please try again!');
+    }
+}
+
 // tests
+// basic arithmetic
 console.log(add(6, 3));
 console.log(subtract(6, 3));
 console.log(multiply(6, 3));
 console.log(divide(6, 3));
+
+// operation
+console.log(operate(6, '+', 3));
+console.log(operate(6, '-', 3));
+console.log(operate(6, '*', 3));
+console.log(operate(6, '/', 3));
