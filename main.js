274b1ae10c7c2ea923b66597fc79caa4c0f1f8ce
Brian Chei <157544112+brianchei@users.noreply.github.com>
2025-07-03 14:21:18 +0900

    feat: add basic arithmetic functions

--- main.js
+++ main.js
@@ -2,3 +2,29 @@
 Pseudocode
 */
 
+// arithmetic methods
+const add = function(addend, addend2) {
+	let sum = addend + addend2;
+    return sum;
+};
+
+const subtract = function(minuend, subtrahend) {
+	let difference = minuend - subtrahend;
+    return difference;
+};
+
+const multiply = function(multiplicand, multiplier) {
+    let product = multiplicand * multiplier;
+    return product;
+};
+
+const divide = function(dividend, divisor) {
+    let quotient = dividend / divisor;
+    return quotient;
+};
+
+// tests
+console.log(add(6, 3));
+console.log(subtract(6, 3));
+console.log(multiply(6, 3));
+console.log(divide(6, 3));
