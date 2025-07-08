b525296fc9ff508352e9339d6fcdde25f84f962a
Brian Chei <157544112+brianchei@users.noreply.github.com>
2025-07-08 21:17:46 +0900

    fix: ui, logic flow

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ let operand2
 
 // arithmetic methods
 const add = function(addend, addend2) {
-	let sum = addend + addend2;
+	let sum = Number(addend) + Number(addend2);
     return sum;
 };
 
@@ -52,33 +52,71 @@ const operate = function(num1, operator, num2) {
 const popDisplay = function() {
     let result = document.querySelector('.result');
     let btns = document.querySelectorAll('button');
+    let currTerm = 1;
+    let expOperator = '';
+    let term1 = '', term2 = '';
     btns.forEach((curr) => curr.addEventListener('click', () => {
-        result.value += curr.textContent;
-
-        let value = result.value;
-        if (!(Number(value))) {
-            try {
-        let parts = value.split(/[+\-*\/]/);
-        if (!(parts.length < 2)) {
-            let operatorIndex = value.includes('+') ? value.indexOf('+') : value.includes('-') ? value.indexOf('-') : value.includes('*') ? value.indexOf('*') : value.includes('/') ? value.indexOf('/') : null;
-            operand1 = value.slice(0, operatorIndex);
-            operator = value.slice(operatorIndex, operatorIndex + 1);
-            operand2 = value.slice(operatorIndex + 1);
-            result.value = operate(operand1, operator, operand2);
+        let keyPressed = curr.textContent;
+        if (keyPressed === 'CLEAR') {
+            result.value = '';
+            return;
         }
+        if (!(keyPressed === '+' || keyPressed === '-' || keyPressed === '*' || keyPressed === '/')) {
+                if (!(keyPressed === '=')) {
+                    if (!(keyPressed === '.')) {
+                        // # logic
+                        if (!(currTerm === 1)) {
+                            term2 += keyPressed;
+                            result.value += keyPressed;
+                            return;
+                        }
+                        term1 += keyPressed;
+                        result.value += keyPressed;
+                        return;
+                    }
+                    // . logic
+                    if (currTerm === 1 && !(term1.includes('.'))) {
+                        term1 += '.';
+                        result.value += keyPressed;
+                    }
+                    if (currTerm === 2 && !(term2.includes('.'))) {
+                        term2 += '.';
+                        result.value += keyPressed;
+                    }
+                }
+                // = logic
+                if (!(term2 === '')) {
+                    result.value = operate(term1, expOperator, term2);
+                    expOperator = '', term1 = '', term2 = ''; currTerm = 1;
+                    return;
+                }
+                return;
+        }
+        // +-*/ logic
+        // fix operator chosen after another operator
+        if (!(expOperator === '')) {
+            expOperator = keyPressed;
+            result.value = result.value.slice(0, -1);
+            result.value += expOperator;
+            return;
+        }
+        if (!(term1 === '')) {
+            expOperator = keyPressed;
+            result.value += keyPressed;
+            currTerm = 2;
+            return;
+        }
+        term1 = 0;
+        expOperator = keyPressed;
+        result.value += keyPressed;
+        currTerm = 2;
         return;
-    } catch (error) {
-        console.log("Error! Invalid character input!");
-    } finally {
-        return;
-    }
-}
     }));
 }
 
 // perform calculation
 let resultDisplay = document.querySelector('.result');
-let result = resultDisplay.value;
+let result = resultDisplay.keyPressed;
 
 // tests
 // basic arithmetic
@@ -93,3 +131,40 @@ console.log(operate(6, '-', 3));
 console.log(operate(6, '*', 3));
 console.log(operate(6, '/', 3));
 popDisplay();
+
+// previous attempt
+/*
+result.keyPressed += keyPressed;
+let keyPressed = result.keyPressed;
+if (!(Number(keyPressed))) {
+    switch (true) {
+        case keyPressed.includes('+') && (keyPressed.indexOf('+') != keyPressed.length - 1):
+            operator = '+';
+            break;
+        case keyPressed.includes('-') && (keyPressed.indexOf('-') != keyPressed.length - 1):
+            operator = '-';
+            break;
+        case keyPressed.includes('*') && (keyPressed.indexOf('*') != keyPressed.length - 1):
+            operator = '*';
+            break;
+        case keyPressed.includes('/') && (keyPressed.indexOf('/') != keyPressed.length - 1):
+            operator = '/';
+            break;
+        default:
+            throw new Error("HELP");
+    }
+    try {
+        let parts = keyPressed.split(/[+\-*\/]/);
+        let operatorIndex = keyPressed.includes('+') ? keyPressed.indexOf('+') : keyPressed.includes('-') ? keyPressed.indexOf('-') : keyPressed.includes('*') ? keyPressed.indexOf('*') : keyPressed.includes('/') ? keyPressed.indexOf('/') : null;
+        operand1 = keyPressed.slice(0, operatorIndex);
+        operand2 = keyPressed.slice(operatorIndex + 1);
+        result.keyPressed = operate(operand1, operator, operand2);
+        
+        return;
+    } catch (error) {
+        console.log("Error! Invalid character input!");
+    } finally {
+        return;
+    }
+}
+*/
